5ebc0cb0f4e6845a541584bfc7cd84decc68dae6
ninjaa <alkhudrisaid83@gmail.com>
2020-09-11 07:19:15 +0700

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+var reactnative = [
+"Hafif",
+"Dayat",
+"Wandi"
+]
+
+var reactjs = [
+ "yusuf",
+ "Lutfi",
+ "tompel",
+
+]
+var javascriptdivision = reactnative.concat(reactjs)
+console.log(javascriptdivision)
